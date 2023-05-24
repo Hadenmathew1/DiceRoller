644f39a7fb55ae9fb31ee5c39380bcd7b5c63463
hadenmathew1 <hadenmoody9@gmail.com>
2023-05-24 15:34:18 -0600

    update code

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".die").addEventListener("click", roller)
+document.addEventListener("change")
+let dieType = null
+function roller() {
+    dieType = document.querySelector("#dieType").value.substring(1);
+    if (isNaN(dieType)) { return }
+    let dieAmt = document.querySelector("#dieAmt").value
+    if (isNaN(dieAmt)) { return }
+    let dieNum = (Math.random() * dieType + 1) * dieAmt
+    document.querySelector("#dieNum").textContent = parseInt(dieNum)
+}
