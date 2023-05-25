7f0def38c601335793675442ac7e81dcddfdbb55
hadenmathew1 <hadenmoody9@gmail.com>
2023-05-24 20:48:16 -0600

    updot

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 document.querySelector("#roll").addEventListener("click", roller)
-document.addEventListener("change")
-let dieType = null
+document.querySelector("#dieAmt").addEventListener("input", multiplier)
+
 function roller() {
-    dieType = document.querySelector("#dieType").value.substring(1);
+    let total = 0
+    let dieType = document.querySelector("#dieType").value.substring(1);
     if (isNaN(dieType)) { return }
     let dieAmt = document.querySelector("#dieAmt").value
     if (isNaN(dieAmt)) { return }
-    let dieNum = (Math.random() * dieType + 1) * dieAmt
-    document.querySelector("#dieNum").textContent = parseInt(dieNum)
+    for(let i = 0; i < dieAmt; ++i) {
+        let dieNum = parseInt(Math.random() * dieType + 1)
+        total += dieNum
+        document.querySelector("#dieNum" + i).textContent = dieNum
+    }
+    document.querySelector("#rollTotal").textContent = total
+    
+}
+function multiplier(e) { 
+    let dieContainer = document.querySelector("#dieContainer")
+    dieContainer.innerHTML = null
+    for(let i = 0; i < e.target.value; ++i) { 
+        dieContainer.appendChild(dieView(i))
+    }
+
+}
+function dieView(idNum) {
+    let div = document.createElement("div")
+    div.classList.add("die")
+    let span = document.createElement("span")
+    span.id = "dieNum" + idNum
+    div.appendChild(span)
+    span.textContent = "1"
+    return div
 }
